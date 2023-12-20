6546f5591bcd8c0f69deb349bb7e1f4ac260dc4b
joejoe2 <7013601@gmail.com>
2023-12-20 21:05:59 +0800

    Update SampleApplication.java

--- src/main/java/com/joejoe2/sample/SampleApplication.java
+++ src/main/java/com/joejoe2/sample/SampleApplication.java
@@ -1,8 +1,6 @@
 package com.joejoe2.sample;
 
 import java.util.Locale;
-import java.util.TimeZone;
-import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,10 +9,5 @@ public class SampleApplication {
   public static void main(String[] args) {
     Locale.setDefault(Locale.ENGLISH);
     SpringApplication.run(SampleApplication.class, args);
-  }
-
-  @PostConstruct
-  public void init() {
-    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 }
